9e835a644d333c1154c5042282777076b29f03d0
Moody8Alt <MudhaffarNgwaliAlt@gmail.com>
2026-03-09 12:09:18 -0400

    Initial commit

---
+++ Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CharacterSelect : MonoBehaviour
+{
+    // Keeps track of which character is selected
+    // When true, player 1 is set as the character Lu
+    public bool P1IsLu { get; private set; } = true;
+
+    // USS Class Names for changing p1/p2 selector positions
+    private const string LeftCharacterSelectClassName = "leftCharacterSelected";
+    private const string RightCharacterSelectClassName = "rightCharacterSelected";
+
+    // Action Map Names
+    private const string UiSelectNamae = "Navigate";
+    private const string UiConfirmName = "Submit";
+
+    // Components
+    private PlayerInput _playerInput;
+
+    // UXML Elements
+    private VisualElement _root;
+    private VisualElement _p1Selector;
+    private VisualElement _p2Selector;
+
+
+    private void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+        _root = GetComponent<UIDocument>().rootVisualElement;
+
+        _p1Selector = _root.Q<VisualElement>("P1Selector");
+        _p2Selector = _root.Q<VisualElement>("P2Selector");
+    }
+
+    private void OnEnable()
+    {
+        _playerInput.actions[UiSelectNamae].started += SwapCharacters;
+        _playerInput.actions[UiConfirmName].started += ConfirmCharacters;
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.actions[UiSelectNamae].started -= SwapCharacters;
+        _playerInput.actions[UiConfirmName].started -= ConfirmCharacters;
+    }
+
+    private void SwapCharacters(InputAction.CallbackContext ctx)
+    {
+        //Debug.Log("P1: " + (_p1IsLu ? "Lu, P2: Mona" : "Mona, P2: Lu"));
+
+        if (P1IsLu)
+        {
+            _p1Selector.RemoveFromClassList(LeftCharacterSelectClassName);
+            _p2Selector.RemoveFromClassList(RightCharacterSelectClassName);
+
+            _p1Selector.AddToClassList(RightCharacterSelectClassName);
+            _p2Selector.AddToClassList(LeftCharacterSelectClassName);
+        }
+        else
+        {
+            _p1Selector.AddToClassList(LeftCharacterSelectClassName);
+            _p2Selector.AddToClassList(RightCharacterSelectClassName);
+
+            _p1Selector.RemoveFromClassList(RightCharacterSelectClassName);
+            _p2Selector.RemoveFromClassList(LeftCharacterSelectClassName);
+        }
+
+        P1IsLu = !P1IsLu;
+    }
+
+    private void ConfirmCharacters(InputAction.CallbackContext ctx)
+    {
+        //Debug.Log("Character Selected!");
+
+        StartCoroutine(Transition());
+
+        SceneManager.LoadScene("Game");
+    }
+
+    /*
+     * TODO: Add a nice transition
+     */
+    private IEnumerator Transition()
+    {
+        yield return new WaitForSeconds(0.1f);
+    }
+}
